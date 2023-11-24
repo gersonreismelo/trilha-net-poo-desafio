12f53b0c94a95661b06fe011db19abb9b1a66422
Gerson dos Reis de Melo <gerson.jp.otaku@gmail.com>
2023-11-24 16:28:25 -0300

    fix: Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Testando a classe Iphone
+Smartphone iphone = new Iphone("123456789", "iPhone 13", "ABCD1234", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+// Testando a classe Nokia
+Smartphone nokia = new Nokia("987654321", "Nokia 3310", "EFGH5678", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
